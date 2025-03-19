c91adf76566bca3383aa08396695423b8e4bfc58
Kevin Nicholas Nathanael <108829565+kevvnnn@users.noreply.github.com>
2025-03-19 07:28:16 +0700

    Update beiju.py

--- w3-queue/beiju.py
+++ w3-queue/beiju.py
@@ -1,31 +1,8 @@
 text = input()
-beiju_chars = [] # queue
-# normal_chars = [] # queue
-temp = [] # stack
-# A deque should be a better implementation
-# bracket_status = 0
+beiju_chars = ""
+normal_chars = ""
+temp = ""
 
-for char in text:
-    # When encountering a closing bracket, append all chars inside bracket to beiju_chars
-    if char == "]": # Beware about brackets entering beiju/normal!
-        for i in range(len(temp)-1,-1,-1):
-            if temp[i] != "[":
-                beiju_chars.append(temp.pop())
-            elif temp[i] == "[":
-                temp.pop()
-                break # Ensures one loop of iterating over chars inside bracket per closing bracket encountered
-    
-    else:
-        temp.append(char)
-    
-    print(temp)
-    # Where to add normal_chars?
-
-# print(*beiju_chars)
-print(*temp)
-# Now ur problem is to print them in the correct order
-
-'''
 for char in text:
     if char == "[":
         bracket_status += 1
@@ -39,4 +16,4 @@ for char in text:
     else:
         beiju_chars += char
 
-print(beiju_chars + normal_chars)'''
+print(beiju_chars + normal_chars)
